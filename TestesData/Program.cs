13ae7b3c178893d9d44080f1f4a13d94ebd4e054
Wallyson Alessandro <wallyson.a3@gmail.com>
2021-11-17 19:10:54 -0300

    First

---
+++ TestesData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestesData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data");
+
+            DateTime dateForButton = DateTime.Now;
+            dateForButton = dateForButton.AddDays(-30);
+
+            Console.WriteLine($"{dateForButton}");
+
+            Console.WriteLine(DateTime.Now.AddSeconds(1));
+        }
+    }
+}
